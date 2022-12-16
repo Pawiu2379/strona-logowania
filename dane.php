6e10d1c6faf22d639731b9142ba8e80fa1dd51dd
Pawiu__ <pawelkoc681@gmail.com>
2022-12-16 22:20:01 +0100

    new feathures

--- dane.php
+++ dane.php
@@ -1,6 +1,6 @@
 <?php 
     $name = $_POST['name'];
-    $secondName = $_POST['$secondName'];
+    $secondName = $_POST['secondName'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $news = $_POST['news'];
@@ -9,31 +9,33 @@
     $postCode = $_POST['postCode'];
     $city = $_POST['city'];
     $country = $_POST['country'];
-    $number = $_POST['number'];
+    $number = $_POST['phone'];
+    $news = boolval($news);
 
+    
+    echo "$name $secondName $email $password $news $company $street $postCode $city $country $number"; 
     //mysql connestion
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "userNames";
+    $dbname = "usernames";
     $tablename = "usersData";
     // Create connection
     $connection = mysqli_connect($servername, $username, $password, $dbname);
     // Check connection
-    if (!$conn) {
+    if (!$connection) {
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    $sql = "INSERT INTO $tablename (name, secondName,email,password,news,company,street,postCode,city,country,number)
-        VALUES ($name, $secondName,$email,$password,$news,$company,$street,$postCode,$city,$country,$number)";
-
-    if (mysqli_query($conn, $sql)) {
+    $sql =" INSERT INTO usersData(`name`, `secondName`,`email`,`password`,`news`,`company`,`street`,`postCode`,`city`,`country`,`number`)
+            VALUES ('$name', '$secondName','$email','$password',$news,'$company','$street',$postCode,'$city','$country',$number)";
+    if (mysqli_query($connection, $sql)) {
         echo "<script>console.log('New record created successfully')</script>";
     } else {
         echo "<script>console.log('Error: " . $sql . "<br>" . mysqli_error($conn) + "')<script>";
     }
 
-    mysqli_close($conn);
+    mysqli_close($connection);
 ?>
 
 
